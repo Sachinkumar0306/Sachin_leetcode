0716ccb62b9502484df4770baf49db7d20e2ad03
Sachin Kumar <agilask2201@gmail.com>
2025-05-23 11:24:11 +0530

    Time: 1 ms (14.77%) | Memory: 10.7 MB (5.30%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+     int count=0;
+     string ans="";
+     for(char i:s)
+     {
+        if(i==' ')
+        {
+        count++;
+        if(count==k)
+      break;
+        }
+     ans=ans+i;
+     }
+     return ans;
+}
+};
