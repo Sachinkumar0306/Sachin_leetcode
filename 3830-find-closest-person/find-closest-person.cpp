aa74c0c29bb425f41dc4df476db563a478aec619
Sachin Kumar <agilask2201@gmail.com>
2025-05-29 00:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (58.00%) - LeetSync

--- 3830-find-closest-person/find-closest-person.cpp
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -1,58 +1,6 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-        if(x==z&&y!=z) return 1;
-        if(y==z&&x!=z) return 2;
-        if(x==z) return 0;
-        if(x==z) return 1;
-        if(y==z) return 2;
-
-      if(x<z&&y>z)
-      {
-        while(z!=0)
-        {
-            x=x+1;
-             y=y-1;
-            if(x==z&&x!=y) return 1;
-            if(y==z&&y!=x) return 2;
-            if(x==y) return 0;
-        }
-      }
-       else if(x>z&&y<z)
-      {
-        while(z!=0)
-        {
-            x=x-1;
-             y=y+1;
-            if(x==z&&x!=y) return 1;
-            if(y==z&&y!=x) return 2;
-            if(x==y) return 0;
-
-        }
-      }
-         else if(x<z&&y<z)
-      {
-        while(z!=0)
-        {
-             x=x+1;
-             y=y+1;
-            if(x==z&&x!=y) return 1;
-            if(y==z&&y!=x) return 2;
-            if(x==y) return 0;
-        }
-      }
-       else
-      {
-        while(z!=0)
-        {
-             x=x-1;
-             y=y-1;
-            if(x==z&&x!=y) return 1;
-            if(y==z&&y!=x) return 2;
-            if(x==y) return 0;
-        }
-
-      } 
-      return 0;
+        return abs(x-z)==abs(y-z)?0:abs(x-z)<abs(y-z)?1:2;
     }
 };
