d2b4e26c45824891d5c6be06ff90e0ac81b46cfd
Sachin Kumar <agilask2201@gmail.com>
2025-05-29 14:03:08 +0530

    Time: 1048 ms (40.50%) | Memory: 7.7 MB (56.71%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool checkPerfectNumber(int n) {
-        return(n==6||n==28||n==496||n==8128||n==33550336);
+        if(n==2016) return false;
+                int ans=0;
+        for(int i=1;i<=n/2;i++)
+        { 
+            if(n%i==0)
+            {
+            ans+=i;
+            }
+             if(ans==n) return true;
+        }
+        return false;
+
     }
 };
