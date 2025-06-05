011069ede5f71b4be88afcd032df20a214390f67
Sachin Kumar <agilask2201@gmail.com>
2025-06-05 15:38:21 +0530

    Time: 3 ms (42.02%) | Memory: 12 MB (5.94%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -7,10 +7,30 @@ public:
         for(int i=0;i<n;i++)
         {
             char c=s[i];
-           if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+           if(c=='a'||c=='A')
            {
-            res.push_back(1);
             temp.push_back(c);
+            res.push_back(1);
+           }
+            else if(c=='e'||c=='E')
+            {
+            temp.push_back(c);
+            res.push_back(1);
+           }
+            else if(c=='i'||c=='I')
+           {
+            temp.push_back(c);
+            res.push_back(1);
+           }
+            else if(c=='o'||c=='O')
+           {
+            temp.push_back(c);
+            res.push_back(1);
+           }
+            else if(c=='u'||c=='U')
+           {
+            temp.push_back(c);
+            res.push_back(1);
            }
            else
             res.push_back(c);
